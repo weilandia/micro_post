f25dba26bc0b31db4675111bd865e02a40da6329
Nick Weiland <nickweiland@gmail.com>
2016-03-20 19:36:52 -0400

    add user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   before_save { self.email = email.downcase }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :password, presence: true, length: { minimum: 6 }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true,
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
+
+  resources :users
+
   get 'about', to: 'static_pages#about'
   get 'help', to: 'static_pages#help'
   get 'contact', to: 'static_pages#contact'
