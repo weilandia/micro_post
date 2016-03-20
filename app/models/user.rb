8396e1e7384382474ae7212cfdba77de8d9f4ecf
Nick Weiland <nickweiland@gmail.com>
2016-03-20 18:45:24 -0400

    add complete user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\z/i
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+end
