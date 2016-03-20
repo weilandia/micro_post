d48e30bced5a7e7e59fe56c5d1164afb48cede0c
Nick Weiland <nickweiland@gmail.com>
2016-03-20 15:07:45 -0400

    update sass

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Micro Post"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Micro Post"
+  end
 end
